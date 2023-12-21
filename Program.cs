e0d0dc79f51ed93063f30d9eb579f93ed518a9cf
Ashish Kumar Singh <singhashish@microsoft.com>
2023-12-21 15:20:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using FlaUI.UIA3;
+
+var app = FlaUI.Core.Application.Launch(@"D:\\repos\\Dec2023\\Win11ThemeSampleApp\\bin\\x64\\Debug\\net9.0-windows\\win-x64\\Win11ThemeSampleApp.exe");
+using (var automation = new UIA3Automation())
+{
+    var window = app.GetMainWindow(automation);
+    Console.WriteLine(window.Title);
+}
